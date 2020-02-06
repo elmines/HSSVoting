74ffd574dd15cdaa217218617dbcfd2cfea2489c
Ethan Mines <elmines325@gmail.com>
2020-02-05 20:18:33 -0600

    WIP: Let network nodes talk to teach other via function calls

--- hss/client.py
+++ hss/client.py
@@ -1,2 +1,23 @@
 class VotingClient(object):
-    pass
+    def __init__(self, repo):
+        self._repo = repo
+        self._shares = []
+        self._outputs = []
+
+    @property
+    def shares(self):
+        return [el for el in self._shares]
+
+    def addShare(self, x):
+        encrypted = self._enc(x)
+        self._shares.append(encrypted)
+
+    def receiveOutputs(y):
+        self._outputs.extend(y)
+
+    def _dec(self):
+        pass
+
+    def _enc(self, x):
+        # TODO: Encrypt the data
+        return x
--- hss/server.py
+++ hss/server.py
@@ -1,2 +1,18 @@
+from typing import Iterable
+
 class VotingServer(object):
-    pass
+
+    def __init__(self, repo, ek=5*7):
+        """
+        :param ek: The evaluation key
+        """
+        self._ek = ek
+
+        # Public variable
+        self.shares = []
+
+    def Eval(self):
+        y_j = None # TODO: Compute the actual return value
+        self.shares = []
+        return y_j
+
