bb581b57c951975b937c0ad4ff67869e396fd485
Ethan Mines <elmines325@gmail.com>
2020-03-29 13:08:02 -0500

    Remove redundant str() calls

--- hss/prf.py
+++ hss/prf.py
@@ -5,13 +5,13 @@ from .elgamal import cryptosystem, enc_elgamal
 def PRFGen():
 	#get binary string of security parameter 1^λ
 	def φ(identifier:int,g : int):
-		id_bits = str(bin(identifier))[2:] 
+		id_bits = bin(identifier)[2:] 
 		λ = len(id_bits)
-		g_length=len(str(bin(g)))-2
+		g_length=len(bin(g))-2
 		for i in range(λ):
 			random.seed(g)
 			rand_bits = random.getrandbits(2*g_length)
-			G = str(bin(rand_bits))[2:].zfill(2*g_length)
+			G = bin(rand_bits)[2:].zfill(2*g_length)
 			if id_bits[λ-1-i] == "0":
 				g= int(G[0 : g_length],2)
 			elif id_bits[λ-1-i] == "1":
