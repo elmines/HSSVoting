87dd0c978c04ea081861a1159aa8fe6958715b01
Ethan Mines <elmines325@gmail.com>
2020-03-31 08:49:41 -0500

    Perform rms load instruction

--- hss/rms.py
+++ hss/rms.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 # Local
 from .types import *
-from .shares import mult_shares, convert_shares
+from .shares import mult_shares, convert_shares, bit_length
 
 StaticContext  = namedtuple("StaticContext", ["G", "M", "φ", "δ_prime"])
 RuntimeContext = namedtuple("RuntimeContext", ["b", "instr_no"])
@@ -10,36 +10,37 @@ RuntimeContext = namedtuple("RuntimeContext", ["b", "instr_no"])
 def concat_bits(x: int, y: int):
     return (x << len(bin(y)[2:])) + y
 
-def rms_mult(w: Ciphertext, y: MemoryVal, statCon, runCon):
-    (G, M, φ, δ_prime) = statCon
-    (b, instr_no) = runCon
-
-    (w_enc, bitwise_encs) = w
-    (y_share, cy_share) = y
-    y_share = int(y_share)
-    cy_share = int(cy_share)
-    
-    wy_mult_share = mult_shares(w_enc, y_share, cy_share)
-    wy_add_share  = convert_shares(b, wy_mult_share, concat_bits(instr_no, 0), δ_prime, M, G, φ)
-    
-    cwy_bitwise_shares = []
-    for (t, bitwise_enc) in enumerate(bitwise_encs):
-        bitwise_mult_share = mult_shares(bitwise_enc, y_share, cy_share)
-        cwy_bitwise_shares.append( convert_shares(b, bitwise_mult_share, concat_bits(instr_no,t), δ_prime, M, G, φ) )
-    cwy_add_share = sum( map(lambda t: 2**t * cwy_bitwise_shares[t], range(len(cwy_bitwise_shares))) )
-    return (wy_add_share, cwy_add_share)
-
 class Evaluator(object):
-    def __init__(G: ModularGroup, program: List[RMSOp], φ, M: int, δ: float):
+    def __init__(self, G: ModularGroup, program: List[RMSOp], φ, M: int, δ: float):
         self.G = G
         self.program = program
         self.M = M
-        self.δ_prime = δ / ((l+1) * M * S)
         self.φ = φ
+
+        l = bit_length(G.generator)
+        self.δ_prime = δ / ((l+1) * M * self.S)
 
     @property
     def S(self):
         return len(self.program)
+
+    def rms_mult(self, w: Ciphertext, y: MemoryVal, b: int, instr_no: int):
+        (G, M, φ, δ_prime) = (self.G, self.M, self.φ, self.δ_prime)
+        (w_enc, bitwise_encs) = w
+        (y_share, cy_share) = y
+        y_share = int(y_share)
+        cy_share = int(cy_share)
+        
+        wy_mult_share = mult_shares(w_enc, y_share, cy_share)
+        wy_add_share  = convert_shares(b, wy_mult_share, concat_bits(instr_no, 0), δ_prime, M, G, φ)
+        
+        cwy_bitwise_shares = []
+        for (t, bitwise_enc) in enumerate(bitwise_encs):
+            bitwise_mult_share = mult_shares(bitwise_enc, y_share, cy_share)
+            cwy_bitwise_shares.append( convert_shares(b, bitwise_mult_share, concat_bits(instr_no,t), δ_prime, M, G, φ) )
+        cwy_add_share = sum( map(lambda t: 2**t * cwy_bitwise_shares[t], range(len(cwy_bitwise_shares))) )
+        return (wy_add_share, cwy_add_share)
+
 
     def public_key_eval(self, b, ek: EK, ct: List[Ciphertext]):
         """
@@ -55,32 +56,31 @@ class Evaluator(object):
     
         instr_no = 1
 
-        def _apply_add(self, instr: RMSAdd):
-            (_, k, i, j) = instr
-            (y_i, cy_i) = memory[i]
-            (y_j, cy_j) = memory[j]
-            memory[k] = (y_i+y_j,cy_i+cy_j)
-        def _apply_mult(self, instr: RMSMult):
-            (_, k, i, j) = instr
-            memory[k] = rms_mult(ct[i], memory[j])
-        def _apply_load(self, instr: RMSLoad):
-            (_, j, i) = instr
-            memory[j] = rms_mult(ct[i], memory[ONE])
-        def _apply_out(self, instr: RMSOut):
-            (_, i) = instr
-            (y_share, cy_share) = memory[i]
-            z_share = y_share
-            #FIXME: Add this in later once we know everything else is working
-            #offset = (self.φ)(self.instr_no,G.generator) 
-            #if b == 1: z_share = z_share - offset
-            #else:      z_share = z_share + offset
-            outputs.append(z_share)
-            
-        op_dict = { "add": lambda op: self.apply_add(op),  "mult": lambda op: self.apply_mult(op),
-                   "load": lambda op: self.apply_load(op),  "out": lambda op: self.apply_out(op)
-        }
-        for op in program:
-            op_dict[op[0]](op)
+        for (op, *operands) in program:
+            if min(operands) < 0:
+                raise Exception("Negative indices are reserved for the library programmer: {(op,*operands)}")
+
+            if op == "load":
+                (j, i) = operands
+                memory[j] = self.rms_mult(ct[i], memory[ONE])
+            elif op == "mult":
+                (k, i, j) = operands
+                memory[k] = rms_mult(ct[i], memory[j])
+            elif op == "add":
+                (k, i, j) = operands
+                (y_i, cy_i) = memory[i]
+                (y_j, cy_j) = memory[j]
+                memory[k] = (y_i+y_j,cy_i+cy_j)
+            elif op == "out":
+                (y_share, cy_share) = memory[i]
+                z_share = y_share
+                #FIXME: Add this in later once we know everything else is working
+                #offset = (self.φ)(self.instr_no,G.generator) 
+                #if b == 1: z_share = z_share - offset
+                #else:      z_share = z_share + offset
+                outputs.append(z_share)
+            else:
+                raise Exception(f"Invalid RMS instruction {op}")
             instr_no += 1
         return outputs
 
--- unit_tests/rms.py
+++ unit_tests/rms.py
@@ -1,4 +1,5 @@
 import unittest
+import random
 
 from hss import *
 
@@ -12,39 +13,34 @@ class TestRMS(unittest.TestCase):
         self.assertEqual(concat_bits(x,y), 0b10101111)
 
     def test_rms_load(self, iterations=10):
-        pass
         correct = 0
         print()
         for i in range(iterations):
             correct += self._rms_load_trial()
-            #print(f"{correct}/{i+1} correct")
+        self.assertTrue(correct > 0) #FIXME: Use a better probability bound than this
 
     def _rms_load_trial(self):
         (pk, ek0, ek1, φ) = gen(16)
         (G, e, one_enc, c_encs) = pk
-
-        w = 1
-        M = 2
-
-        one0 = ek0[1]
-        one1 = ek1[1]
-        c0   = ek0[2]
-        c1   = ek1[2]
-
+        δ = math.exp(-5)
+        M = 5
+        servers = Evaluator(G, ["placeholder", "placeholder"], φ, M, δ)
+        (one0, one1) = (ek0[1], ek1[1])
+        (c0, c1) = (ek0[2], ek1[2])
         mem0 = (one0, c0)
         mem1 = (one1, c1)
 
         c = c0 + c1
+        w = random.randrange(M)
         (w_enc, prod_encs) = enc(pk, w)
         self.assertEqual(w, dec_elgamal(G, c, w_enc))
         ct = (w_enc, prod_encs)
 
-        δ = math.exp(-6)
         instr_no = 10
-        statCon = StaticContext(G=G, φ=φ, M=M, δ_prime=δ)
 
-        out0 = rms_mult(ct, mem0, statCon, RuntimeContext(0, instr_no))
-        out1 = rms_mult(ct, mem1, statCon, RuntimeContext(0, instr_no))
+        out0 = servers.rms_mult(ct, mem0, 0, instr_no)
+        out1 = servers.rms_mult(ct, mem1, 1, instr_no)
+
         w_cand = out0[0] + out1[0]
         cw_cand = out0[1] + out1[1]
 
