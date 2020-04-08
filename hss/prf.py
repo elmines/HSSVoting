29f546c59f1e80d9707eb215a98820f88b680059
Ethan Mines <elmines325@gmail.com>
2020-04-08 14:31:46 -0500

    Use c's srand()

--- hss/prf.py
+++ hss/prf.py
@@ -1,3 +1,5 @@
+from ctypes import CDLL
+import random
 from random import Random
 from .types import *
 from .elgamal import cryptosystem, enc_elgamal
@@ -5,18 +7,28 @@ from .elgamal import cryptosystem, enc_elgamal
 def PRFGen(grp: ModularGroup):
     g_length = len(bin(grp.divisor)[2:])
     right_mask = 2**g_length - 1 # A binary string of ones
-
+    rng = CRandom()
     def φ(identifier: int, g: ModularInt):
         id_bits = bin(identifier)[2:] 
         λ = len(id_bits)
         g_val = g.value
         for i in range(λ):
-            rand_bits = Random(g.value).getrandbits(2*g_length)
+            rng.seed(g_val)
+            rand_bits = rng.getrandbits(2*g_length)
             b = (identifier >> i) & 1
             if b: g_val = rand_bits & right_mask  # The right half of rand_bits
             else: g_val = (rand_bits >> g_length) # The left half of rand_bits
         return ModularInt(g_val,grp.order) # Output must be l bits, so take g_val % G.order
     return φ
+
+class CRandom(object):
+    def __init__(self):
+        self._libc = CDLL("libc.so.6")
+        pass
+    def seed(self,s: int):
+        self._libc.srand(s)
+    def getrandbits(self,n: int):
+        return self._libc.random() >> (32 - n)
 
 
 def Get_phi_prime(identifier:int,φ):
